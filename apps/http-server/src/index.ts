6126c29ad7bd9d5791929a972f0cbb0437382b6e
Utkarsh <kutkarsh.umar@gmail.com>
2025-03-09 11:35:59 +0530

    package/backend-common

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import { JWT_SECRET } from './config'
+import { JWT_SECRET } from '@repo/backend-common/config'
 import { auth } from './middleware/auth'
 
 const app = express()
